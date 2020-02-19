1e9969455e6daafc1a3587be53ceefcea89262d2
Kyle <43458322+kyle-stevens@users.noreply.github.com>
2020-02-19 12:12:50 -0700

    some basic formatting and line changes

--- DEPTH_SENSOR/depther_sensor/depth_sensor.ino
+++ DEPTH_SENSOR/depther_sensor/depth_sensor.ino
@@ -5,24 +5,43 @@
 
 ros::NodeHandle nh;
 
-void callback_func(const std_msgs::String& mesg){
+void callback_func(const std_msgs::String& mesg)
+{
    char c = Wire.read(); 
    Serial.println(c);
 }
+
+
 ros::Publisher depth_info("depth_sensor", &mesg);
 
-void setup(){
-  nh.initNode();
-  nh.advertise(depth_info);
-  int count =0;
-  Serial.begin(9600);
+
+void setup()
+{
+   
+   nh.initNode();
+   
+   nh.advertise(depth_info);
+   
+   int count =0;
+   
+   Serial.begin(9600);
+
 }
-void loop(){
-  count++;
-  mesg = "Hello" + count.str();
-  depth_info.publish(&mesg);
-  nh.spinOnce();
-  Serial.println(mesg);
-  delay(500);
+
+
+void loop()
+{
+   
+   count++;
+   
+   mesg = "Hello" + count.str();
+   
+   depth_info.publish(&mesg);
+   
+   nh.spinOnce();
+   
+   Serial.println(mesg);
+   
+   delay(500);
   
 }
