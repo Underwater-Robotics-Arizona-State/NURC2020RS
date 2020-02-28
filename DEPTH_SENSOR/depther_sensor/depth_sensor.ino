6d3eb7829ad36c014385d10fd12c3a54869df841
Kyle <43458322+kyle-stevens@users.noreply.github.com>
2020-02-27 19:15:37 -0700

    ADDED COMMENTS
    on nearly every damn line

--- DEPTH_SENSOR/depther_sensor/depth_sensor.ino
+++ DEPTH_SENSOR/depther_sensor/depth_sensor.ino
@@ -1,41 +1,38 @@
-#include <ros.h>
-#include <std_msgs/Float64>
-#include <Wire.h>
-#include "MS5837.h"
-
-//This comment is enough commenting, right Panda?
-
-ros::NodeHandle nh;
-
-std_msgs::Float32 depth_val;
-
-ros::Publisher depth_info("depth_vals", &depth_val);
-
-MS5837 sensor;
-
-void setup(){
-	nh.initNode();
-	nh.advertise(depth_info);
-	
-	Wire.begin();
-
-	while(!sensor.init()){
-		delay(5000);
-	}	
-	sensor.setModel(MS5837::MS5837_30BA);
-	sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)	
-}
-
-
-void loop(){
-	sensor.read();
-
-	depth_val.data = sensor.depth();
-
-	nh.spinOnce();
-
-	if(depth_val != NULL){ 
-		depth_info.publish(&depth_val);
-	}
-	delay(500);
-}
+#include <ros.h>					//IMPORT ROS LIBRARY
+#include <std_msgs/Float64>				//IMPORT ROS MESSAGES FLOAT 64 FOR DEPTH VALUES
+#include <Wire.h>					//IMPORT WIRE LIBRARY FOR I2C
+#include "MS5837.h"					//IMPORT DEPTH SENSOR LIBRARY
+							//
+ros::NodeHandle nh;					//CREATE NODE HANDLE OBJECT FOR PUBLISHING FLOAT VALUES
+							//
+std_msgs::Float32 depth_val;				//CREATE FLOAT VALUE VARIABLE TO PUBLISH TO ROSTOPIC
+							//
+ros::Publisher depth_info("depth_vals", &depth_val);	//CREATE PUBLISHER OBJECT THAT PUBLISHES TO "depth_vals" ROSTOPIC
+							//
+MS5837 sensor;						//CREATE SENSOR OBJECT FOR DEPTH SENSOR
+							//
+void setup(){						//ARDUINO SETUP FUNCTION
+	nh.initNode();					//INITIALIZE ROS NODE
+	nh.advertise(depth_info);			//ADVERTISE ROS NODE
+							//
+	Wire.begin();					//BEGIN WIRE TRANSMISSION
+							//
+	while(!sensor.init()){				//CHECK FOR DEPTH SENSOR
+		delay(5000);				//WAIT 5 SECONDS BEFORE TRYING AGAIN
+	}						//
+	sensor.setModel(MS5837::MS5837_30BA);		//SET MODEL OF DEPTH SENSOR
+	sensor.setFluidDensity(997); 			//kg/m^3 (freshwater, 1029 for seawater)	
+}							//
+							//
+void loop(){						//ARDUINO LOOP FUNCTION
+	sensor.read();					//UPDATE SENSOR VALUES
+							//
+	depth_val.data = sensor.depth();		//GET DEPTH SENSOR DEPTH VALUE
+							//
+	nh.spinOnce();					//SPIN ROS NODE HANDLER
+							//
+	if(depth_val != NULL){ 				//CHECK IF 'depth_val' IS NULL
+		depth_info.publish(&depth_val);		//PUBLISH DEPTH VALUES
+	}						//
+	delay(500);					//WAIT 0.5 SECONDS (WILL BE DEPRECATED FOR ACTUAL USE)
+}							//
